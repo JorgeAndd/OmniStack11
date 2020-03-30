8854d20a8131600cebd4b18007f0ba93e9adec7b
JorgeAndd <jorgeluizandrade@outlook.com>
2020-03-29 21:16:18 -0300

    Add basic unit and integration test using jest

--- backend/src/controllers/NgoController.js
+++ backend/src/controllers/NgoController.js
@@ -1,5 +1,5 @@
+const generateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
-const crypto = require('crypto');
 
 module.exports = {
     async index(request, response) {
@@ -9,7 +9,7 @@ module.exports = {
     },
 
     async create(request, response) {
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
         const { name, email, whatsapp, city, uf } = request.body;
 
         await connection('ngo').insert({
